4567d5c81338b048909119988ae006b64d4736fb
Ivander Carlos Dias <ivander@buildbox.com.br>
2023-08-06 18:23:47 -0300

    fix database directory

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { knex as knexQueryBuilder, Knex } from 'knex'
 export const config: Knex.Config = {
    client: 'sqlite',
    connection: {
-      filename: './tmp/app.db',
+      filename: './database/app.db',
    },
    useNullAsDefault: true,
    migrations: {
