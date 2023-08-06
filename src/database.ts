7eff91d8f1208c92ca486d85a03685c65422d4cd
Ivander Carlos Dias <ivander@buildbox.com.br>
2023-08-06 18:16:13 -0300

    config knex migration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as knexQueryBuilder } from 'knex'
+import { knex as knexQueryBuilder, Knex } from 'knex'
 
-export const knex = knexQueryBuilder({
+export const config: Knex.Config = {
    client: 'sqlite',
    connection: {
       filename: './tmp/app.db',
    },
-})
+   useNullAsDefault: true,
+   migrations: {
+      extension: 'ts',
+      directory: './database/migrations',
+   },
+}
+
+export const knex = knexQueryBuilder(config)
