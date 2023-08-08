191e36e4aa304869072bbff0b3c876a14a7a4f34
Ivander Carlos Dias <ivander@buildbox.com.br>
2023-08-08 08:37:47 -0300

    listing routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+   app.get('/', async () => {
+      const transactions = await knex('transactions').select()
+
+      return { transactions }
+   })
+
+   app.get('/:id', async (req) => {
+      const getTransactionsParamsSchema = z.object({
+         id: z.string().uuid(),
+      })
+
+      const { id } = getTransactionsParamsSchema.parse(req.params)
+
+      const transaction = await knex('transactions').where('id', id).first()
+
+      return { transaction }
+   })
+
    app.post('/', async (req, res) => {
       const createTransactionBodySchema = z.object({
          title: z.string(),
